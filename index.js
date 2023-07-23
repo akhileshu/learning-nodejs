061d77be2dfa1ac8f602c298d130cd378253313a
Akhilesh Kumar Upadhyay <umapati4381@gmail.com>
2023-07-23 16:13:40 +0530

    advanced dynamic html to show all products

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ import { readFileSync } from "fs";
 
 let htmlPage = readFileSync("index.html", "utf-8");
 const data = JSON.parse(readFileSync("data.json", "utf-8"));
-const product=data.products[0]
+// const product = data.products[0];
 
-// dynamic html 
-  htmlPage=htmlPage.replace('**title**',product.title).replace('**price**',product.price).replace('**rating**',product.rating).replace('**url**',product.thumbnail)
+// dynamic html
+// htmlPage=htmlPage.replace('**title**',product.title).replace('**price**',product.price).replace('**rating**',product.rating).replace('**url**',product.thumbnail)
 
+// more advanced dynamic html to show all products
+const products = data.products;
+// Function to generate the HTML for each product
+function generateProductHTML(product) {
+  return `
+    <div class="product">
+      <h2>${product.title}</h2>
+      <p>Price: ${product.price}</p>
+      <p>Rating: ${product.rating}</p>
+      <img src="${product.thumbnail}" alt="${product.title}">
+    </div>
+  `;
+}
+
+// Replace the placeholder in the HTML with all the product items
+const allProductsHTML = products.map(generateProductHTML).join("");
+htmlPage = htmlPage.replace("<!-- Placeholder for product items -->", allProductsHTML);
 
 // Create a local server
 const server = createServer((req, res) => {
