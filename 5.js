544b427d289eaec3f4823991536cc304b17f78a2
Akhilesh Kumar Upadhyay <umapati4381@gmail.com>
2023-07-26 15:11:16 +0530

    final to deploy on versal

--- 5.js
+++ 5.js
@@ -1,33 +1,36 @@
 /*
 learning model view controller,file structure
 */
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const mongoose = require("mongoose");
+// middleware for cross conection
+const cors = require("cors");
 const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
 const server = express();
-console.log('env',process.env.DB_PASSWORD);
+const path =require('path')
+console.log("env", process.env.DB_PASSWORD);
 
 // db connection code
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/employes');
-  console.log('db connected')
+  // await mongoose.connect("mongodb://127.0.0.1:27017/employes");
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("db connected");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
-
-
-  
-
 // body parser
 server.use(express.json());
+server.use(cors());
+server.use(express.static(process.env.PUBLIC_DIR))
+server.use("/products", productRouter.router);
+server.use("/users", userRouter.router);
+server.use('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'build','index.html'))
+})
 
-server.use('/products' , productRouter.router)
-server.use('/users' , userRouter.router)
-
-server.listen(process.env.PORT,()=>console.log('server started'));
+server.listen(process.env.PORT, () => console.log("server started"));
--- model/product.js
+++ model/product.js
@@ -4,10 +4,10 @@ const { Schema } = mongoose;
 // schema
 const productSchema = new Schema({
     title: String, // String is shorthand for {type: String}
-    description: {type:String ,required:true ,unique:true},
+    description: {type:String ,unique:true},
     price: {type:Number,min:[0,'wrong price']},
     discountPercentage: {type:Number,min:[0,'wrong min disc'],max:[50,'wrong max disc']},
-    rating: {type:Number,min:[0,'wrong min r'],max:[5,'wrong max r']},
+    rating: {type:Number,min:[0,'wrong min r'],max:[5,'wrong max r'],default:0},
     brand: {type:String ,required:true},
     category: {type:String ,required:true},
     thumbnail: {type:String ,required:true},
